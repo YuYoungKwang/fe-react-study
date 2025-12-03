0d628d83872ba69171507e2230dd685e73a65549
YuYoungKwang <db99817@gmail.com>
2025-12-03 12:31:03 +0900

    리액트 기본구조, JSX 기본 사용법

---
+++ src/TempApp.js
@@ -0,0 +1,16 @@
+import './TempApp.css';
+
+
+// react 방식 js -> 함수 형식 컴포넌트
+function TempApp(){
+    return(
+        <div>
+            <h1>TempApp Component</h1>
+            <h2>CSS Test</h2>
+        </div>
+    );
+}
+
+export default TempApp;
+
+//<TempApp/>
