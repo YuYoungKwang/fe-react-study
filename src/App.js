2c45d92e9b7bb5421161aa1e7cea0f2220c9e25e
YuYoungKwang <db99817@gmail.com>
2025-12-04 16:30:50 +0900

    1. NewsBlog 페이지 기본세팅 2. State변수 기반 Modal 창 표시여부 처리 3. event.stopPropagation()

--- src/App.js
+++ src/App.js
@@ -10,10 +10,12 @@ import Props01 from './components/Props01';
 import Props02 from './components/Props02';
 import Quiz02 from './quiz/quiz02/Quiz02';
 import Quiz03 from './quiz/quiz03/Quiz03';
+import NewsBlog from './newsblog/NewsBlog';
+
 function App() {
 
     // return <Export01/>;
-    return <Quiz03/>;
+    return <NewsBlog/>;
 
   // return (
   //   <div className="App">
